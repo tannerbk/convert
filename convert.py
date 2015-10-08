c4e995da6615f99675381c33d01ab87ff5b27d9e
Tanner Kaptanoglu <tannerk@sas.upenn.edu>
2015-10-08 16:46:15 -0400

    Improve interface w/ user.

--- convert.py
+++ convert.py
@@ -2,69 +2,72 @@
 import sys 
 import numpy as np
 import h5py
-import os 
- 
-def parse_file(anin, j): 
-    '''
-    Parse the ASCII data file to extract relevant information. 
-    Assumed number of sample (1024) and number of events/file (500)
-    are constants. You can extract the following from the converted
-    .hdf5 file: 
-    -- Array of voltages per channel  
-    -- Length of each trace 
-    -- Total number of events 
-    '''
- 
-    # Count the number of lines -> events 
-    with open(anin) as a:
+''' Convert ASCII CAEN files to .hdf5 files ''' 
+
+''' Count files ''' 
+def count_files(ascii_filename):
+    with open(ascii_filename) as a:
         for i, l in enumerate(a):
             pass
-    events = i + 1
+    files = i + 1
+    return files
 
-    samples = 1024 
-    total_events = events*500
-    print total_events 
-    voltage = np.zeros([total_events, samples])
+''' Parse the ASCII data file to extract relevant information.
+Assumed number of sample (1024) and number of events/file (1000)
+are constants.
+'''
+def parse_file(ascii_filename, channel_number):
+    samples = 1024
+    files = count_files(ascii_filename)
+    total_events = files*1000
+    print total_events
+    voltage = np.empty([total_events, samples], dtype='f')
     eventcounter = 0
     
-    ''' Loop through every file 
-        Get each filename 
-        For each filename, open each line
-        Look for "Ch: %d in each line
-        If its there, the voltage info is next line 
-        Convert voltage array in list of floats 
-        Keep track of event number 
-        Return voltage array 
-        Size (total events, samples)
-    '''
-    with open(anin) as ff:
+    with open(ascii_filename) as ff:
         filename = ff.readlines()
         for k in range(0,len(filename)):
             fline = filename[k]
             fline = fline.strip()
-            print fline, 
-            print '(CH %d) \r' % j 
-            with open(fline) as f: 
+            print fline
+            with open(fline) as f:
                 lines = f.readlines()
                 for i in range(0,len(lines)):
-                    line = lines[i]  
-                    line = line.strip()     
-                    if ('CH: %d' % j) in line: 
-                        line2 = lines[i+1] 
-                        k = 0 
+                    line = lines[i]
+                    line = line.strip()
+                    if ('CH: %d' % channel_number) in line:
+                        line2 = lines[i+1]
+                        samplecounter = 0
                         for dpoint in line2.split():
                             dpoint = float(dpoint)
-                            voltage[eventcounter,k] = dpoint 
-                            k += 1
+                            voltage[eventcounter,samplecounter] = dpoint
+                            samplecounter += 1
                             continue
                         eventcounter += 1
     return voltage
 
+''' Save to an hdf5 dataset '''
+def get_dataset(f, ascii_filename, num_channels):
+     files = count_files(ascii_filename)
+     events = files*1000
+     samples = 1002
+     for i in range(0,num_channels+1):
+         channel_name = 'channel%d' % i
+         print channel_name
+         channel_info = parse_file(ascii_filename,i)
+     dataset = f.create_dataset(channel_name,data=channel_info,dtype='f4')
+     channel_info = np.empty([events,samples])
+     return dataset
+
+''' Accept user inputs '''
 if (__name__=="__main__"):
-     # Get array of voltages for each channel 
-     channel0 = parse_file("anin.text",0)
-     channel1 = parse_file("anin.text",1)
-     f = h5py.File("PMYQ.hdf5",'w')
-     dset0 = f.create_dataset("channel0",data=channel0,dtype='f8')     
-     dset1 = f.create_dataset("channel1",data=channel1,dtype='f8')
+     import argparse
+     parser = argparse.ArgumentParser(description='Accept ASCII file to convert.')
+     parser.add_argument('ascii_filename', help='Name of ASCII File')
+     parser.add_argument('hdf5_filename', help='Name of HDF5 file')
+     parser.add_argument('num_channels', type=int, help='Number of channels')
+     args = parser.parse_args()
+
+     f = h5py.File(args.hdf5_filename,'w')
+     get_dataset(f,args.ascii_filename,args.num_channels)
      f.close()
